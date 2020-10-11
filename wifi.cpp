0580b0de7eb2aff4a89879ce10ae31f9eb2171cd
Faizan Khalid <IamFaizanKhalid@gmail.com>
2020-10-11 16:01:00 +0500

    Removed creation of temporary file

--- wifi.cpp
+++ wifi.cpp
@@ -1,29 +1,58 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
 using namespace std;
-#include<fstream>
-#include<string.h>
+
+string exec(const char* cmd) {
+    char buffer[128];
+    string result = "";
+    FILE* pipe = _popen(cmd, "r");
+    if (!pipe) throw runtime_error("_popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        _pclose(pipe);
+        throw;
+    }
+    _pclose(pipe);
+    return result;
+}
+
 void saveProfileNames()
 {
 	system("t.cmd");
 	system("cls");
 	//save all profiles name in out.txt
 }
+
 int main()
 {
 	saveProfileNames();
 	ifstream ifs("out.txt");
 	string data;
 	string temp;
+	vector<string> output;
 	while(getline(ifs,data))
 	{
 		temp="netsh wlan show profile \"";
 		temp+=data;
-		temp+="\" key=clear >> swp.txt";
-		system(temp.data());
+		temp+="\" key=clear";
+		output.push_back(exec(temp.data()));
 	}
 	ifs.close();
 	remove("out.txt");
-	remove("temp.txt");
-	rename("swp.txt","saved-wifi-passwords.txt");
+
+	ofstream ofs("saved-wifi-passwords.txt");
+	for (string str: output)
+		ofs << str;
+	ofs.close();
+	
 	return 0;
 }
